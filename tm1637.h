15929fe8a4c19e400ef18de2f2ca10df5191656d
SergeyVN94 <blakdgak1@yandex.ru>
2020-04-18 18:30:51 +0300

    Removed unused ADDRESSES structure.

--- tm1637.h
+++ tm1637.h
@@ -28,23 +28,6 @@ class TM1637 {
 
     // normal mode, fixed address, write data to display register
     const uint8_t DISPLAY_CONFIGD = 0x44;
-
-    const struct {
-        const uint8_t C0H;
-        const uint8_t C1H;
-        const uint8_t C2H;
-        const uint8_t C3H;
-        const uint8_t C4H;
-        const uint8_t C5H;
-    } ADDRESSES = {
-        C0H : 0xC0,
-        C1H : 0xC1,
-        C2H : 0xC2,
-        C3H : 0xC3,
-        C4H : 0xC4,
-        C5H : 0xC5,
-    };
-
     const uint8_t DISPLAY_BRIGHT[9] = {
         0x80, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F,
     };
